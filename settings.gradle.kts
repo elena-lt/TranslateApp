7721c9426fb0a95c0bdde2a54f40d506a40355b9
Lena Lutsiuk <elenalutsiuk5@gmail.com>
2023-09-03 22:33:55 +0200

    Update GitHub Actions workflow

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,7 +23,7 @@ pluginManagement {
 
 dependencyResolutionManagement {
   versionCatalogs {
-    create("libs") { from(files("../Transaltor_KMM/gradle/wrapper/libraries.versions.toml"))}
+    create("libs") { from(files("../Transaltor_KMM/gradle/wrapper/libs.versions.toml"))}
   }
 
   repositories {
