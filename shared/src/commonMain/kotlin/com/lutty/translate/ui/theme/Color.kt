7baab4ec13e57386d61e69abfded7540efb2b16b
Lena Lutsiuk <elenalutsiuk5@gmail.com>
2023-09-03 19:40:13 +0200

    Add app colors, theme and typography

---
+++ shared/src/commonMain/kotlin/com/lutty/translate/ui/theme/Color.kt
@@ -0,0 +1,63 @@
+package com.lutty.translate.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+//Light scheme
+val PurplePrimaryColorLight = Color(0xffb90063)
+val OnPurplePrimaryColorLight = Color(0xffffffff)
+val PrimaryPurpleContainerColorLight = Color(0xffffd9e2)
+val OnPrimaryPurpleContainerColorLight = Color(0xff3e001d)
+
+val SecondaryPurpleColorLight = Color(0xff4565f)
+val OnPurpleSecondaryColorLight = Color(0xffffffff)
+val PurpleSecondaryContainerColorLight = Color(0xffffd9e2)
+val OnPurpleSecondaryContainerColorLight = Color(0xff2b151c)
+
+val PurpleTertiaryColorLight = Color(0xff924276)
+val OnPurpleTertiaryColorLight = Color(0xffffffff)
+val PurpleTertiaryContainerColorLight = Color(0xffffd8eb)
+val OnPurpleTertiaryContainerColorLight = Color(0xff3b002c)
+
+val ErrorLight = Color(0xffba1a1a)
+val OnErrorLight = Color(0xffffffff)
+val ErrorContainerLight = Color(0xffffdad6)
+val OnErrorContainerLight = Color(0xff410002)
+
+val BackgroundLight = Color(0xfffffbff)
+val OnBackgroundLight = Color(0xff201a1b)
+val SurfaceLight = BackgroundLight
+val OnSurfaceLight = OnBackgroundLight
+val SurfaceVariantLight = Color(0xfff2dde1)
+val OnSurfaceVariantLight = Color(0xff514347)
+
+val OutlineLight = Color(0xff837377)
+
+//Dark scheme
+val PurplePrimaryColorDark = Color(0xffffb1c8)
+val OnPurplePrimaryColorDark = Color(0xff650033)
+val PrimaryPurpleContainerColorDark= Color(0xff8e004a)
+val OnPrimaryPurpleContainerColorDark = Color(0xffffd9e2)
+
+val SecondaryPurpleColorDark = Color(0xff4565f)
+val OnPurpleSecondaryColorDark = Color(0xff422931)
+val PurpleSecondaryContainerColorDark = Color(0xff422931)
+val OnPurpleSecondaryContainerColorDark = Color(0xffffd9e2)
+
+val PurpleTertiaryColorDark = Color(0xffffaedd)
+val OnPurpleTertiaryColorDark = Color(0xff5a1145)
+val PurpleTertiaryContainerColorDark = Color(0xff752a5d)
+val OnPurpleTertiaryContainerColorDark = Color(0xffffd8eb)
+
+val ErrorDark = Color(0xffffb4ab)
+val OnErrorDark = Color(0xff690005)
+val ErrorContainerDark = Color(0xff93000a)
+val OnErrorContainerDark = Color(0xffffdad6)
+
+val BackgroundDark = Color(0xff201a1b)
+val OnBackgroundDark = Color(0xffebe0e1)
+val SurfaceDark = BackgroundLight
+val OnSurfaceDark = OnBackgroundLight
+val SurfaceVariantDark= Color(0xff514347)
+val OnSurfaceVariantDark = Color(0xffd5c2c6)
+
+val OutlineDark = Color(0xff9e8c90)
---
+++ shared/src/commonMain/kotlin/com/lutty/translate/ui/theme/Theme.kt
@@ -0,0 +1,102 @@
+package com.lutty.translate.ui.theme
+
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import com.lutty.translate.ui.theme.BackgroundDark
+import com.lutty.translate.ui.theme.BackgroundLight
+import com.lutty.translate.ui.theme.ErrorContainerDark
+import com.lutty.translate.ui.theme.ErrorContainerLight
+import com.lutty.translate.ui.theme.ErrorDark
+import com.lutty.translate.ui.theme.ErrorLight
+import com.lutty.translate.ui.theme.OnBackgroundDark
+import com.lutty.translate.ui.theme.OnBackgroundLight
+import com.lutty.translate.ui.theme.OnErrorContainerDark
+import com.lutty.translate.ui.theme.OnErrorContainerLight
+import com.lutty.translate.ui.theme.OnErrorDark
+import com.lutty.translate.ui.theme.OnErrorLight
+import com.lutty.translate.ui.theme.OnPrimaryPurpleContainerColorDark
+import com.lutty.translate.ui.theme.OnPrimaryPurpleContainerColorLight
+import com.lutty.translate.ui.theme.OnPurplePrimaryColorDark
+import com.lutty.translate.ui.theme.OnPurplePrimaryColorLight
+import com.lutty.translate.ui.theme.OnPurpleSecondaryColorDark
+import com.lutty.translate.ui.theme.OnPurpleSecondaryColorLight
+import com.lutty.translate.ui.theme.OnPurpleSecondaryContainerColorDark
+import com.lutty.translate.ui.theme.OnPurpleSecondaryContainerColorLight
+import com.lutty.translate.ui.theme.OnPurpleTertiaryColorDark
+import com.lutty.translate.ui.theme.OnPurpleTertiaryColorLight
+import com.lutty.translate.ui.theme.OnPurpleTertiaryContainerColorDark
+import com.lutty.translate.ui.theme.OnPurpleTertiaryContainerColorLight
+import com.lutty.translate.ui.theme.OnSurfaceDark
+import com.lutty.translate.ui.theme.OnSurfaceLight
+import com.lutty.translate.ui.theme.OnSurfaceVariantDark
+import com.lutty.translate.ui.theme.OnSurfaceVariantLight
+import com.lutty.translate.ui.theme.OutlineDark
+import com.lutty.translate.ui.theme.OutlineLight
+import com.lutty.translate.ui.theme.PrimaryPurpleContainerColorDark
+import com.lutty.translate.ui.theme.PrimaryPurpleContainerColorLight
+import com.lutty.translate.ui.theme.PurplePrimaryColorDark
+import com.lutty.translate.ui.theme.PurplePrimaryColorLight
+import com.lutty.translate.ui.theme.PurpleSecondaryContainerColorDark
+import com.lutty.translate.ui.theme.PurpleSecondaryContainerColorLight
+import com.lutty.translate.ui.theme.PurpleTertiaryColorDark
+import com.lutty.translate.ui.theme.PurpleTertiaryColorLight
+import com.lutty.translate.ui.theme.PurpleTertiaryContainerColorDark
+import com.lutty.translate.ui.theme.PurpleTertiaryContainerColorLight
+import com.lutty.translate.ui.theme.SecondaryPurpleColorDark
+import com.lutty.translate.ui.theme.SecondaryPurpleColorLight
+import com.lutty.translate.ui.theme.SurfaceDark
+import com.lutty.translate.ui.theme.SurfaceLight
+import com.lutty.translate.ui.theme.SurfaceVariantDark
+import com.lutty.translate.ui.theme.SurfaceVariantLight
+
+val DarkColorScheme = darkColorScheme(
+  background = BackgroundDark,
+  error = ErrorDark,
+  errorContainer = ErrorContainerDark,
+  onBackground = OnBackgroundDark,
+  onError = OnErrorDark,
+  onErrorContainer = OnErrorContainerDark,
+  onPrimary = OnPurplePrimaryColorDark,
+  onPrimaryContainer = OnPrimaryPurpleContainerColorDark,
+  onSecondary = OnPurpleSecondaryColorDark,
+  onSecondaryContainer = OnPurpleSecondaryContainerColorDark,
+  onSurface = OnSurfaceDark,
+  onSurfaceVariant = OnSurfaceVariantDark,
+  onTertiary = OnPurpleTertiaryColorDark,
+  onTertiaryContainer = OnPurpleTertiaryContainerColorDark,
+  outline = OutlineDark,
+  primary = PurplePrimaryColorDark,
+  primaryContainer = PrimaryPurpleContainerColorDark,
+  secondary = SecondaryPurpleColorDark,
+  secondaryContainer = PurpleSecondaryContainerColorDark,
+  surface = SurfaceDark,
+  surfaceVariant = SurfaceVariantDark,
+  tertiary = PurpleTertiaryColorDark,
+  tertiaryContainer = PurpleTertiaryContainerColorDark
+)
+
+val LightColorTheme = lightColorScheme(
+  background = BackgroundLight,
+  error = ErrorLight,
+  errorContainer = ErrorContainerLight,
+  onBackground = OnBackgroundLight,
+  onError = OnErrorLight,
+  onErrorContainer = OnErrorContainerLight,
+  onPrimary = OnPurplePrimaryColorLight,
+  onPrimaryContainer = OnPrimaryPurpleContainerColorLight,
+  onSecondary = OnPurpleSecondaryColorLight,
+  onSecondaryContainer = OnPurpleSecondaryContainerColorLight,
+  onSurface = OnSurfaceLight,
+  onSurfaceVariant = OnSurfaceVariantLight,
+  onTertiary = OnPurpleTertiaryColorLight,
+  onTertiaryContainer = OnPurpleTertiaryContainerColorLight,
+  outline = OutlineLight,
+  primary = PurplePrimaryColorLight,
+  primaryContainer = PrimaryPurpleContainerColorLight,
+  secondary = SecondaryPurpleColorLight,
+  secondaryContainer = PurpleSecondaryContainerColorLight,
+  surface = SurfaceLight,
+  surfaceVariant = SurfaceVariantLight,
+  tertiary = PurpleTertiaryColorLight,
+  tertiaryContainer = PurpleTertiaryContainerColorLight
+)
