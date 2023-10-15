160823eaf19b0b3c655674c16de921fd3b5fd813
Lena Lutsiuk <elenalutsiuk5@gmail.com>
2023-10-15 11:19:24 +0200

    Display history on translation screen

--- androidApp/src/main/java/com/lutty/translate/android/translate/presentation/TranslateScreen.kt
+++ androidApp/src/main/java/com/lutty/translate/android/translate/presentation/TranslateScreen.kt
@@ -9,18 +9,23 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.unit.dp
 import com.lutty.translate.android.R
 import com.lutty.translate.android.translate.presentation.components.LanguageDropdown
 import com.lutty.translate.android.translate.presentation.components.SwapLanguagesButton
+import com.lutty.translate.android.translate.presentation.components.TranslateHistoryItem
 import com.lutty.translate.android.translate.presentation.components.TranslateTextField
 import com.lutty.translate.android.translate.presentation.components.rememberTextToSpeech
 import com.lutty.translate.core.presentation.TranslateEvent
@@ -28,6 +33,7 @@ import com.lutty.translate.core.presentation.TranslateEvent.ChangeTranslationTex
 import com.lutty.translate.core.presentation.TranslateEvent.CloseTranslation
 import com.lutty.translate.core.presentation.TranslateEvent.EditTranslation
 import com.lutty.translate.core.presentation.TranslateEvent.OpenToLangDropDown
+import com.lutty.translate.core.presentation.TranslateEvent.SelectHistoryItem
 import com.lutty.translate.core.presentation.TranslateEvent.SwapLanguages
 import com.lutty.translate.core.presentation.TranslateState
 import java.util.Locale
@@ -106,6 +112,23 @@ fun TranslateScreen(
             tts.speak(state.toText, TextToSpeech.QUEUE_FLUSH, null, null)
           },
           onTextFieldClick = { onEvent(EditTranslation) },
+          modifier = Modifier.fillMaxWidth()
+        )
+      }
+
+      item {
+        if (state.history.isNotEmpty()) {
+          Text(
+            text = stringResource(id = R.string.history),
+            style = MaterialTheme.typography.h2
+          )
+        }
+      }
+
+      items(state.history) { historyItem ->
+        TranslateHistoryItem(
+          item = historyItem,
+          onClick = { onEvent(SelectHistoryItem(historyItem)) },
           modifier = Modifier.fillMaxWidth()
         )
       }
